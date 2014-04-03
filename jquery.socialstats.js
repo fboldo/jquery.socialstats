43401bb3c23533f9abc8da3ab1ece2864e99b1e9
fboldo <filipeboldo@gmail.com>
2014-04-03 18:16:58 -0300

    Script included

---
+++ jquery.socialstats.js
@@ -0,0 +1,68 @@
+/*
+ * jQuery Socialstats v1.0
+ * https://github.com/hyperbolicbr/jquery.socialstats
+ * Copyright 2014 Hyperbolic
+ */
+
+( function($) {
+
+		$.fn.socialstats = function(options) {
+
+			var settings = $.extend({
+				zeroText : "0",
+				facebookApi : "http://graph.facebook.com/",
+				twitterApi : "https://cdn.api.twitter.com/1/urls/count.json"
+			}, options);
+
+			return this.each(function() {
+
+				var counter;
+				var elem;
+
+				elem = $(this);
+
+				if (!elem.hasClass("socialstats-loaded")) {
+
+					if (elem.attr("data-network") == "facebook") {
+
+						$.getJSON(settings.facebookApi + "?ids=" + encodeURIComponent(elem.attr("data-url")), function(data) {
+
+							if (elem.attr("data-action") == "comments") {
+								counter = data[elem.attr("data-url")].comments;
+							} else if (elem.attr("data-action") == "all") {
+								counter = data[elem.attr("data-url")].shares+data[elem.attr("data-url")].comments;
+							} else {
+								counter = data[elem.attr("data-url")].shares;
+							}
+
+							if ((counter != undefined)) {
+								elem.html(counter);
+							} else {
+								elem.html(settings.zeroText);
+							}
+
+						});
+
+					} else if (elem.attr("data-network") == "twitter") {
+
+						$.getJSON(settings.twitterApi + "?callback=?&url=" + encodeURIComponent(elem.attr("data-url")), function(data) {
+
+							counter = data.count;
+							if ((counter != undefined)) {
+								elem.html(counter);
+							} else {
+								elem.html(settings.zeroText);
+							}
+						});
+
+					}
+
+					elem.addClass("socialstats-loaded");
+
+				}
+
+			});
+
+		};
+
+	}(jQuery)); 
